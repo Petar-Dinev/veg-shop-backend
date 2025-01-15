ceb18ca95aa588836e7ae2e43f08f5096201d21b
Petar-Dinev <p.dinev55@gmail.com>
2025-01-15 14:12:00 +0200

    Refactor user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,26 @@
+const { register, login } = require('../services/userService');
+
 const userController = require('express').Router();
 
 
-userController.post('/register', (req, res) => {
+userController.post('/register', async (req, res) => {
     const { email, username, password } = req.body;
-    console.log(email);
-    res.status(204).end()
+    try {
+        const newUser = await register(email, username, password)
+        res.json(newUser)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 
-userController.post('/login', (req, res) => {
-    console.log('Login successfull', req.body);
-    res.status(204).end()
+userController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const result = await login(email, password)
+        res.json(result)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 
 userController.get('/logout', (req, res) => {
